526d76c929729f3b3a8aaf1049207b5994774dff
stearnsbq <stearnsbq@vcu.edu>
2021-10-09 11:46:54 -0400

    ignore

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,13 @@
 import "reflect-metadata";
 import App from "./app";
 import { json } from "body-parser";
-import Container from "typedi";
-import { MongoService } from "./services/MongoService";
-import { AboutMe } from "./model/AboutMe";
-import { Project } from "./model/Project";
+import mongoose from 'mongoose'; 
 
 const app = new App(__dirname + "/controllers", [json()], 8080);
 
 app.listen(async () => {
 
-  const mongoService = Container.get(MongoService);
-
-  await mongoService.connect("mongodb://192.168.1.247:27017/personal")
+  await mongoose.connect(process.env.MONGO_URL);
 
   console.log(`Listening on ${app.port}`);
 });
